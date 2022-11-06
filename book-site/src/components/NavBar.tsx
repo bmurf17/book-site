c4b30a789767832b7d0c798be89754560faa7dd1
bmurf17 <bmurf17@gmail.com>
2022-11-05 20:10:06 -0400

    Basic Color and styles set

--- book-site/src/components/NavBar.tsx
+++ book-site/src/components/NavBar.tsx
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { Fragment } from "react";
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 const Counter = () => {
-    const [count, setCount] = useState(0)
     return (
-        <div className="flex h-screen">
-            <div className="m-auto">
-                <div className="text-6xl text-red-600">{count}</div>
-                <button className="px-6 py-2 rounded bg-green-800 hover:bg-green-600 text-white" type="button" onClick={() => setCount((count) => count + 1)}>
-                    count+
-                </button>
-            </div>
+        <div className="w-full h-20 flex justify-between items-center px-8 text-black">
+            <h1 className="text-2xl font-bold text-[#4236CE]">REACT</h1>
+            <ul className="flex items-center">
+                <li className="px-4 text-lg font-semibold" >Text</li>
+                <li className="px-4 text-lg font-semibold text-[#E24E1B]">Error</li>
+                <li className="px-4 text-lg font-semibold text-[#0D6E0A]">Interact</li>
+            </ul>
+
         </div>
     )
 }
