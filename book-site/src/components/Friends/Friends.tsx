0462e88539b929b5dd8b5e5e5f9228f4dbe0423c
bmurf17 <bmurf17@gmail.com>
2022-11-20 21:49:53 -0500

    Friends Page Tempalte

--- book-site/src/components/Friends/Friends.tsx
+++ book-site/src/components/Friends/Friends.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 function Friends() {
-    return(
-        <main>
-            Friends
-        </main>
-    )
+  return (
+    <div className="rounded overflow-hidden shadow-lg bg-white w-full">
+      <div className="grid grid-cols-1 divide-y px-6 py-4">
+        <div className="flex ">Add Friends</div>
+        <div>Someone Read a book</div>
+        <div>Someone added a new friend</div>
+        <div>Some new joined the website</div>
+      </div>
+    </div>
+  );
 }
 
 export default Friends;
