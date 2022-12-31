a4d9feb91979b00143e6b99555a0ebaad36df47c
bmurf17 <bmurf17@gmail.com>
2022-12-31 14:37:00 -0500

    Added in basics for allowing firebase google auth

--- book-site/src/App.tsx
+++ book-site/src/App.tsx
@@ -1,17 +1,23 @@
 import Nav from "./components/Nav";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+import React, { useState } from "react";
 
 const queryClient = new QueryClient();
+const UserContext = React.createContext(null);
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Nav />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <UserContext.Provider value={user}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Nav />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </UserContext.Provider>
     </div>
   );
 }
--- book-site/src/components/Nav.tsx
+++ book-site/src/components/Nav.tsx
@@ -116,53 +116,75 @@ function Nav() {
                   leaveTo="transform opacity-0 scale-95"
                 >
                   <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="px-1 py-1 ">
-                      <Link to={"myaccount"}>
+                    {false ? (
+                      <>
+                        <div className="px-1 py-1 ">
+                          <Link to={"myaccount"}>
+                            <Menu.Item>
+                              {({ active }) => (
+                                <button
+                                  className={`${
+                                    active
+                                      ? "bg-green-600 text-white"
+                                      : "text-gray-900"
+                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                >
+                                  My Account
+                                </button>
+                              )}
+                            </Menu.Item>
+                          </Link>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <button
+                                className={`${
+                                  active
+                                    ? "bg-green-600 text-white"
+                                    : "text-gray-900"
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                              >
+                                Settings
+                              </button>
+                            )}
+                          </Menu.Item>
+                        </div>
+                        <div className="px-1 py-1">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Link to={"login"}>
+                                <button
+                                  className={`${
+                                    active
+                                      ? "bg-violet-500 text-white"
+                                      : "text-gray-900"
+                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                >
+                                  Logout
+                                </button>
+                              </Link>
+                            )}
+                          </Menu.Item>
+                        </div>
+                      </>
+                    ) : (
+                      <div className="px-1 py-1 ">
                         <Menu.Item>
                           {({ active }) => (
-                            <button
-                              className={`${
-                                active
-                                  ? "bg-green-600 text-white"
-                                  : "text-gray-900"
-                              } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                            >
-                              My Account
-                            </button>
+                            <Link to={"login"}>
+                              <button
+                                className={`${
+                                  active
+                                    ? "bg-violet-500 text-white"
+                                    : "text-gray-900"
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                              >
+                                Login
+                              </button>
+                            </Link>
                           )}
                         </Menu.Item>
-                      </Link>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <button
-                            className={`${
-                              active
-                                ? "bg-green-600 text-white"
-                                : "text-gray-900"
-                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                          >
-                            Settings
-                          </button>
-                        )}
-                      </Menu.Item>
-                    </div>
-                    <div className="px-1 py-1">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link to={"login"}>
-                            <button
-                              className={`${
-                                active
-                                  ? "bg-violet-500 text-white"
-                                  : "text-gray-900"
-                              } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                            >
-                              Logout
-                            </button>
-                          </Link>
-                        )}
-                      </Menu.Item>
-                    </div>
+                      </div>
+                    )}
                   </Menu.Items>
                 </Transition>
               </Menu>
