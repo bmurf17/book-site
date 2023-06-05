68b64a1486a6fd49d347788de3854ab92ae58197
Brendan Murphy <brendan.murphy@ostusa.com>
2023-06-05 13:03:52 -0400

    Adjust Nav bar to reflect User being logged in

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { Fragment, useState } from "react";
+import { Fragment, useContext, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { routes } from "../Constants/Routes";
 import { faChartLine } from "@fortawesome/free-solid-svg-icons";
@@ -10,6 +10,7 @@ import { UserContext } from "../App";
 
 function Nav() {
   const [open, setOpen] = useState(false);
+  const { user } = useContext(UserContext);
 
   return (
     <>
@@ -103,7 +104,7 @@ function Nav() {
               <Menu as="div" className="relative inline-block text-left">
                 <div>
                   <Menu.Button className="inline-flex w-full justify-center rounded-md bg-green-600 bg-opacity-100 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                    User
+                    {user ? <>{user.name}</> : <>User</>}
                   </Menu.Button>
                 </div>
                 <Transition
@@ -116,7 +117,7 @@ function Nav() {
                   leaveTo="transform opacity-0 scale-95"
                 >
                   <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    {false ? (
+                    {user ? (
                       <>
                         <div className="px-1 py-1 ">
                           <Link to={"myaccount"}>
